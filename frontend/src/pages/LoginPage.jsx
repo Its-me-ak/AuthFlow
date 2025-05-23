24ee80e1ec844462861bdd830132570fcda7d4de
Mohd Aquib <akibkhan9984@gmail.com>
2025-05-23 23:06:36 +0530

    feat: enhance LoginPage with login functionality and error handling; update auth store for login and email verification

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,13 +3,24 @@ import { motion, } from "motion/react"
 import Input from '../components/Input'
 import { Mail, Lock, Loader } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import { useAuthStore } from '../store/authStore'
 
 
 
 const LoginPage = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
- const isLoading = false
+
+  const { login, isLoading, error } = useAuthStore()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      await login(email, password)
+    } catch (error) {
+      console.error('Error logging in:', error)
+    }
+  }
 
   return (
     <motion.div
@@ -18,11 +29,11 @@ const LoginPage = () => {
       transition={{ duration: 0.5 }}
       className="max-w-md w-full bg-gray-800/50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
     >
-      <div className='p-8'>
-        <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 bg-clip-text text-transparent'>
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 bg-clip-text text-transparent">
           Welcome Back
         </h2>
-        <form >
+        <form onSubmit={handleLogin}>
           <Input
             icon={Mail}
             type="email"
@@ -42,36 +53,46 @@ const LoginPage = () => {
             autoComplete="off"
           />
 
-          <div className='flex items-center mb-2'>
-            <Link to="/forgot-password" className='text-sm text-green-500 hover:underline'>
+          <div className="flex items-center mb-2">
+            <Link
+              to="/forgot-password"
+              className="text-sm text-green-500 hover:underline"
+            >
               Forgot Password
             </Link>
           </div>
+          {error && (
+            <p className="text-red-500 text-sm mt-2 font-medium">{error}</p>
+          )}
 
           <motion.button
             type="submit"
-            className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
           font-bold rounded-lg shadow-lg hover:from-green-600
           hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
-           focus:ring-offset-gray-900 transition duration-200'
+           focus:ring-offset-gray-900 transition duration-200"
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
             disabled={isLoading}
           >
-            {
-              isLoading ? <Loader className='w-6 h-6 animate-spin mx-auto' /> : "Login"
-            }
+            {isLoading ? (
+              <Loader className="w-6 h-6 animate-spin mx-auto" />
+            ) : (
+              "Login"
+            )}
           </motion.button>
         </form>
       </div>
-      <div className='px-8 py-4 bg-gray-900/50 flex justify-center'>
-        <p className='text-sm text-gray-400'>
+      <div className="px-8 py-4 bg-gray-900/50 flex justify-center">
+        <p className="text-sm text-gray-400">
           Don't have an account ?
-          <Link to="/signup" className='text-green-500 hover:underline ml-2'>Sign Up</Link>
+          <Link to="/signup" className="text-green-500 hover:underline ml-2">
+            Sign Up
+          </Link>
         </p>
       </div>
     </motion.div>
-  )
+  );
 }
 
 export default LoginPage
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -6,43 +6,86 @@ const API_URL = "http://localhost:8080/api/v1/auth"
 axios.defaults.withCredentials = true
 
 export const useAuthStore = create((set) => ({
-    user: null,
-    isAuthenticated: false,
-    error: null,
-    isLoading: false,
-    isCheckingAuth: true,
+  user: null,
+  isAuthenticated: false,
+  error: null,
+  isLoading: false,
+  isCheckingAuth: true,
 
-    signup: async (name, email, password) => {
-        try {
-            set({isLoading: true, error: null})
-            const response = await axios.post(`${API_URL}/signup`, {name, email, password})
-            set({user: response.data.user, isAuthenticated: true, isLoading: false})
-        } catch (error) {
-            set({error: error.response.data.message || "Error signing up", isLoading: false})
-            throw error
-        }
-    },
+  signup: async (name, email, password) => {
+    try {
+      set({ isLoading: true, error: null });
+      const response = await axios.post(`${API_URL}/signup`, {
+        name,
+        email,
+        password,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error signing up",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
 
-    verifyEmail: async (verificationCode) => {
-        try {
-            set({isLoading: true, error: null})
-            const response = await axios.post(`${API_URL}/verify-email`, { verificationCode })
-            set({user: response.data.user, isAuthenticated: true, isLoading: false})
-            return response.data
-        } catch (error) {
-            set({error: error.response.data.message || "Error verifying email", isLoading: false})
-            throw error
-        }
-    },  
+  login: async (email, password) => {
+    try {
+      set({ isLoading: true, error: null });
+      const response = await axios.post(`${API_URL}/login`, {
+        email,
+        password,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error logging in",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
 
-    checkAuth: async () => {
-        try {
-            set({isCheckingAuth: true, error: null})
-            const response = await axios.get(`${API_URL}/check-auth`)
-            set({user: response.data.user, isAuthenticated: true, isCheckingAuth: false})
-        } catch (error) {
-            set({error: null, isCheckingAuth: false,})
-            throw error
-        }
-    },
-}))
+  verifyEmail: async (verificationCode) => {
+    try {
+      set({ isLoading: true, error: null });
+      const response = await axios.post(`${API_URL}/verify-email`, {
+        verificationCode,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+      return response.data;
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error verifying email",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
+    try {
+      const response = await axios.get(`${API_URL}/check-auth`);
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isCheckingAuth: false,
+      });
+    } catch (error) {
+      set({ error: null, isCheckingAuth: false, isAuthenticated: false });
+    }
+  },
+}));
