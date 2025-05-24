09cb26b00e0c8f236ed91fc0c16a073df0382aaa
Mohd Aquib <akibkhan9984@gmail.com>
2025-05-24 18:36:05 +0530

    chore: update package-lock.json and package.json for dependency management

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,36 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { connectDatabase } from "./db/connectDB.js";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
 app.use(express.json()); // Middleware to parse incoming JSON requests. It makes req.body contain the parsed data.
 app.use(cookieParser()); // Middleware to parse incoming cookies in req.cookies object for authentication purposes
 
-app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/auth", authRoutes);
 
+if (process.env.VITE_NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
-    connectDatabase()
-    console.log(`Server is running on port ${PORT}`);
-})
+  connectDatabase();
+  console.log(`Server is running on port ${PORT}`);
+});
