a9a9a584d352d5902c8fc24ef840291b8d99a58a
Mohd Aquib <akibkhan9984@gmail.com>
2025-06-02 22:46:54 +0530

    fix: correct NODE_ENV variable usage in index.js and package.json

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser()); // Middleware to parse incoming cookies in req.cookies 
 
 app.use("/api/v1/auth", authRoutes);
 
-if (process.env.VITE_NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
